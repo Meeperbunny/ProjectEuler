bd44cce5d88150becad4691d1713e31575ede7ad
Meeperbunny <mckibbenian@gmail.com>
2024-07-19 11:07:21 -0600

    Solved 153

--- scripts/153.py
+++ scripts/153.py
@@ -20,38 +20,21 @@ def divsum(n):
         upper = (norm_sum(cnt) - norm_sum(i))
     #     print(i, lower, upper)
         tot += lower + upper
-        tot %= mod
     return tot
-
-def ans(n):
-    seen = set()
-    g = 1
-    tot = [0 for _ in range(n + 1)]
-    while g <= n:
-        if g < 1000:
-            print(g)
-        if g % 10000 == 0:
-            print(g / n)
-        a = g
-        while a * a  <= n * g:
-            b = g
-            while a * a + b * b <= n * g:
-                imp_d = (a * a + b * b) // g
-                # new_g = gcd(a, b)
-                # if new_g * new_g > n or new_g == g:
-                    # print(a, b, imp_d, (n) // imp_d * 2 * a)
-                    # tot[0] += (n) // imp_d * 2 * a
-                for imp_n in range(imp_d, n + 1, imp_d):
-                    # if (a, b, imp_n) not in seen:
-#                         print('\t', a, b, imp_n)
-                        tot[imp_n] += 2 * a
-                        # seen.add((a, b, imp_n))
-                b += g
-            a += g
-        g += 1
-    return sum(tot)
-n = 100000
-res = ans(n) + divsum(n)
-print("Answer:", res)
-# assert(res == 1304302582)
-# print("Is Good")
+n = int(1e8)
+t = 0
+for g in range(1, n + 1):
+    if g < 1000:
+        print(g)
+    if g % (n // 100) == 0:
+        cn = (g // (n // 100))
+        print("#" * cn + "." * (100 - cn))
+    for a in range(g, n + 1, g):
+        for b in range(g, n + 1, g):
+            if (a * a + b * b) // g > n:
+                break
+            gp = gcd(a, b)
+            if g == gp:
+                nu = (a*a+b*b) // g
+                t += (n // nu) * a * 2
+print(t, t + divsum(n))
