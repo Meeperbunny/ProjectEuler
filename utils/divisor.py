36f3c39377782be73cbccba2f62bc70ef758019d
Ian McKibben <mckibbenian@gmail.com>
2025-04-24 10:26:41 +0900

    problems

--- utils/divisor.py
+++ utils/divisor.py
@@ -5,3 +5,10 @@ def get_divs(n):
         for q in range(i, n + 1, i):
             divs[q].append(i)
     return divs
+
+def combine_divs(d1, d2):
+    dset = set()
+    for a in d1:
+        for b in d2:
+            dset.add(a * b)
+    return list(dset)
