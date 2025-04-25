059655ce1b0727548935d9753537fea5646e8e58
Ian McKibben <mckibbenian@gmail.com>
2025-04-25 20:32:05 +0900

    400 problems done

---
+++ scripts/242.cc
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define HEADER ios::sync_with_stdio(0);cin.tie(0);if (fopen("file.in", "r")) {freopen("file.in", "r+", stdin);};
+#define all(v) v.begin(), v.end()
+using ll = unsigned long long;
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+
+#ifdef IAN_DEBUG
+#define dbg(...) cerr << '[' << __FILE__ << ':' << __LINE__ << "] (" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+int main() {
+    ll n = 1'000'000'000'000ll;
+    ll t = 0;
+    for(ll i = 0; (i * 4) + 1 <= n; i++) {
+        t += (1ll << __builtin_popcountll(i));
+    }
+    cout << t << endl;
+    return 0;
+}
